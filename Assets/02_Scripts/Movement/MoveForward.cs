74e1218388949d23622fb225e9071fc1ad95b39d
max heinze <maxheinze@googlemail.com>
2023-11-25 14:12:58 +0100

    waypoint looping and new terrain tile spawning

--- Assets/02_Scripts/Movement/MoveForward.cs
+++ Assets/02_Scripts/Movement/MoveForward.cs
@@ -2,32 +2,36 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    [SerializeField] Terrain terrain; 
-    [SerializeField] float speed=10f;
+    [SerializeField] bool loop = false;
+    [SerializeField] float speed = 10f;
     [SerializeField] float minHeight = 3f;
     [SerializeField] float rotationSpeed =10f;
     [SerializeField] float slerpDistance = 100f;
+    [SerializeField] GameObject terrainTiles;
+    Terrain terrain;
+    GameObject NextTile;
     Vector3 aimPoint = Vector3.zero;
     WayPoint wayPoint = null;
-    int wayPointNumber = 0;
+    int wayPointNumber, tileNumber = 1;
     public float MapheightAtPos(Vector3 position) { return terrain.SampleHeight(position); }
-    public Vector3 GetAimPoint(int wpNum)
-    { 
-        Vector3 ap = wayPoint.GetItem(wpNum);
-        ap.y = 0;
-        return ap;
-    }
+    public Vector3 GetAimPoint(int wpNum) { return wayPoint.GetItem(wpNum); }
     private void Start()
     {
+        terrain = GetComponentInChildren<Terrain>();
         wayPoint = GetComponent<WayPoint>();
         aimPoint = GetAimPoint(wayPointNumber);
-        transform.position = new Vector3(aimPoint.x, MapheightAtPos(aimPoint), aimPoint.z);
+        //transform.position = new Vector3(aimPoint.x, MapheightAtPos(aimPoint), aimPoint.z);
         wayPointNumber++;
         aimPoint = GetAimPoint(wayPointNumber);
     }
     void Update()
     {
-        if (NextWP()) { wayPointNumber++; aimPoint = GetAimPoint(wayPointNumber); }
+        if (NextWP()) {
+            if (wayPointNumber < wayPoint.lastElement) { wayPointNumber++; }
+            else if (loop) { wayPointNumber = 0; }
+            else HandleNextTerrainTile();
+            aimPoint = GetAimPoint(wayPointNumber); 
+        }
         Aim();
         Vector3 target = transform.position + transform.forward * Time.deltaTime * speed;
         target.y = MapheightAtPos(transform.position) + minHeight;
@@ -44,7 +48,11 @@ public class MoveForward : MonoBehaviour
     float GetHorizontalDistanceToWP()
     {
         Debug.Log(wayPointNumber);
-        if (wayPointNumber < 1) { return 0; }
+        if (wayPointNumber == 0) {
+            float distanceBetweenLastAndNextAimPoint = Vector2.Distance(new Vector2(GetAimPoint(wayPointNumber).x, GetAimPoint(wayPointNumber).z), new Vector2(GetAimPoint(wayPoint.lastElement).x, GetAimPoint(wayPoint.lastElement).z));
+            float currentDistance = Vector2.Distance(new Vector2(aimPoint.x, aimPoint.z), new Vector2(transform.position.x, transform.position.z));
+            return (distanceBetweenLastAndNextAimPoint - currentDistance) / distanceBetweenLastAndNextAimPoint;
+        }
         else
         {
             float distanceBetweenLastAndNextAimPoint = Vector2.Distance(new Vector2(GetAimPoint(wayPointNumber).x, GetAimPoint(wayPointNumber).z), new Vector2(GetAimPoint(wayPointNumber - 1).x, GetAimPoint(wayPointNumber - 1).z));
@@ -56,4 +64,14 @@ public class MoveForward : MonoBehaviour
     {
         return GetHorizontalDistanceToWP() > .9f; 
     }
+    void HandleNextTerrainTile() {
+        Debug.Log("ChangeScene");
+        NextTile = Instantiate(terrainTiles, new Vector3(0, 0, terrain.terrainData.size.z), Quaternion.identity);
+        wayPoint = NextTile.GetComponentInChildren<WayPoint>();
+        wayPoint.Translate(tileNumber);
+        terrain = NextTile.GetComponentInChildren<Terrain>();
+        tileNumber++;
+        wayPointNumber = 0;
+    }
+    
 }
--- Assets/02_Scripts/Path Building/WayPoint.cs
+++ Assets/02_Scripts/Path Building/WayPoint.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 public class WayPoint : MonoBehaviour
 {
     public List<Vector3> waypoints = new List<Vector3>();
-    public int Count { get { return waypoints.Count; } }
+    public int lastElement { get { return waypoints.Count-1; } }
     public Vector3 GetItem(int i) { return waypoints[i]; }
     public void SetItem(int i, Vector3 value) { waypoints[i] = value; }
     public Vector3[] ToArray() { return waypoints.ToArray(); }
+    public void Translate(int tileNum)
+    {
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            waypoints[i] = GetItem(i) + (tileNum *new Vector3(0,0,1000));
+            Debug.Log(waypoints[i]);
+        }
+    }
 }
