b99a3814e4bcadbc7ba3096d7178579988a1b989
max heinze <maxheinze@googlemail.com>
2024-01-05 08:35:56 +0100

    getnearesttarget final updat

--- Assets/02_Scripts/Interactions/GetNearestTarget.cs
+++ Assets/02_Scripts/Interactions/GetNearestTarget.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GetNearestTarget : MonoBehaviour
@@ -16,30 +15,11 @@ public class GetNearestTarget : MonoBehaviour
       var targets = potentials
          .Select(t => t.GetComponent<HealthManager>())
          .Where(t => t != null)
-         .Select(u => u.GetComponent<Transform>())
          .Distinct()
-         .OrderBy(t => Mathf.Abs(Vector3.SignedAngle(aimDirection.forward, t.transform.position,Vector3.up)))
+         .Select(u => u.GetComponent<Transform>())
+         .OrderBy(u => Vector3.Dot(aimDirection.forward, u.transform.position))
          .Take(numberToGet)
          .ToArray();
       return targets.Length > 0 ? targets : null;
-   }
-
-   private void OnDrawGizmos()
-   {
-      if (aimDirection != null)
-      {
-         Gizmos.color = Color.red;
-         Gizmos.DrawRay(aimDirection.position, aimDirection.forward * 1000);
-      }
-
-      var targets = GetTargets(2);
-      Gizmos.color = Color.green;
-      if (targets != null)
-      {
-         foreach (var t in targets)
-         {
-            Gizmos.DrawWireSphere(t.position, 2f);
-         }
-      }
    }
 }
