dcd4192fb8728382888f7bcbc0059a20413ad880
max heinze <maxheinze@googlemail.com>
2024-01-08 13:27:40 +0100

    update to loot code

--- Assets/02_Scripts/Interactions/Loot.cs
+++ Assets/02_Scripts/Interactions/Loot.cs
@@ -6,10 +6,9 @@ public class Loot:MonoBehaviour
     public float scale = 1f;
     public void GetLoot(Vector3 position)
     {
-        float drawn = Random.Range(0f, 100f);
         foreach (LootItem loot in loots)
         {
-            if (drawn <= loot.chance)
+            if (Random.Range(0f, 100f) <= loot.chance)
             {
                 GameObject powerup = Instantiate(loot.powerUp);
                 powerup.GetComponent<LootBehaviour>().Type = loot.type;
