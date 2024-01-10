6682c4b4e06f741af0ecd0e4e01f54a35ca5f06f
max heinze <maxheinze@googlemail.com>
2024-01-10 17:39:17 +0100

    updated movement and enemy behaviour

--- Assets/02_Scripts/Interactions/EnemyBehaviour.cs
+++ Assets/02_Scripts/Interactions/EnemyBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
@@ -5,26 +6,54 @@ public class EnemyBehaviour : MonoBehaviour
     private Transform player = null;
     private bool isTargetted = false;
     private Rigidbody rigidbody;
-    private float speed;    
+    private float speed;
+    private WayPoint wp;
+    private int nearestWp;
     public float Speed { get { return speed; } set { speed = value; } }
     public bool IsTargetted { get => isTargetted; set => isTargetted = value; }
     void Start()
     {
+        wp = FindFirstObjectByType<WayPoint>();
         speed = FindFirstObjectByType<MoveWithPath>().Speed;
         rigidbody = GetComponent<Rigidbody>();
         player = GameObject.FindFirstObjectByType<CrosshairMovement>().transform;
         transform.LookAt(player.position);
+        nearestWp = GetNearestWp();
     }
     // Update is called once per frame
     void Update()
     {
             transform.LookAt(player.position);
-            //if (rigidbody != null) TravelInDirection(pathDirection());
+            if (rigidbody != null) TravelInDirection(PathDirection());
     }
 
     void TravelInDirection(Vector3 pathDirection)
     {
         rigidbody.velocity = pathDirection * speed;
+    }
+
+    Vector3 PathDirection()
+    {
+        if (Vector3.Dot(player.forward, transform.position - player.position) > 0)
+            Debug.Log("I'm in front of player");
+        else
+        {
+            Debug.Log("I'm behind player");
+        }
+        return Vector3.zero;
+    }
+
+    int GetNearestWp()
+    {
+        var nearestWp = wp.waypoints
+            .OrderBy(t => Vector3.Distance(transform.position, t))
+            .First();
+        for (int i = 0; i < wp.waypoints.Count; i ++)
+        {
+            if (wp.GetItem(i) != nearestWp) continue;
+            return i;
+        }
+        return -1;
     }
 
     private void OnTriggerEnter(Collider other)
