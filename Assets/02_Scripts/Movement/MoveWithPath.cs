d2d3a36bb2ac203371cac6e93a4ef6ad29761622
max heinze <maxheinze@googlemail.com>
2024-01-12 12:51:54 +0100

    updated movement scripts to prevent going under world

--- Assets/02_Scripts/Movement/MoveWithPath.cs
+++ Assets/02_Scripts/Movement/MoveWithPath.cs
@@ -2,21 +2,20 @@ using UnityEngine;
 
 public class MoveWithPath : MonoBehaviour
 {
-    [SerializeField] bool loop = false;
+    [SerializeField] bool loop;
     [SerializeField] float speed = 10f;
     [SerializeField] float minHeight = 1f;
     [SerializeField] float maxHeight = 11f;
     [SerializeField] GameObject terrainTiles;
     [SerializeField][Range(0, 1)] float ratio;
     Terrain terrain;
-    GameObject NextTile;
+    GameObject nextTile;
     Vector3 aimPoint = Vector3.zero;
-    WayPoint wayPoint = null;
+    WayPoint wayPoint;
     int wayPointNumber, tileNumber = 1;
-    public float MinHeight { get { return minHeight; } }
-    public float Speed { get { return speed; } set { speed = value; } }
-    public float MapheightAtPos(Vector3 position) { return terrain.SampleHeight(position); }
-    public Vector3 GetAimPoint(int wpNum) { return wayPoint.GetItem(wpNum); }
+    public float Speed { get => speed; set => speed = value; }
+    public float MapheightAtPos(Vector3 position) => terrain.SampleHeight(position);
+
     private void Start()
     {
         terrain = terrainTiles.GetComponentInChildren<Terrain>();
@@ -29,61 +28,65 @@ public class MoveWithPath : MonoBehaviour
     }
     void Update()
     {
-        if (NextWP()) {
+        if (NextWp()) {
             if (wayPointNumber < wayPoint.lastElement) { wayPointNumber++; }
             else if (loop) { wayPointNumber = 0; }
             else HandleNextTerrainTile();
             aimPoint = GetAimPoint(wayPointNumber);
         }
+        var tr = transform;
+        var tPos = tr.position;
         //face aimPoint ignoring y axis as aimpoint always y = 0
-        Quaternion aimPointHorizontalRotation = Quaternion.LookRotation(new Vector3(aimPoint.x, transform.position.y, aimPoint.z) - transform.position);
+        Quaternion aimPointHorizontalRotation = Quaternion.LookRotation(new Vector3(aimPoint.x, tPos.y, aimPoint.z) - tPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, aimPointHorizontalRotation, ratio * Time.deltaTime);
         
         //move toward target using newly calculated forward direction.
-        Vector3 target = transform.position + transform.forward * (Time.deltaTime * speed);
-        target.y = MapheightAtPos(transform.position) + ((minHeight + maxHeight )/2);
+        Vector3 target = tPos + tr.forward * (Time.deltaTime * speed);
+        target.y = MapheightAtPos(tPos) + ((minHeight + maxHeight )/2);
         transform.position = Vector3.Slerp(transform.position, target, speed);
 
         //set tilt? if so normalize terrain gradient to set current tilt
      
         }
+    private Vector3 GetAimPoint(int wpNum) => wayPoint.GetItem(wpNum);
 
     private float GetHorizontalDistanceToNextWayPoint(int wpNumber)
     {
-        if (wpNumber == 0) {
+        if (wpNumber == 0)
+        {
+            var t = transform.position;
             float distanceBetweenLastAndNextAimPoint = Vector2.Distance(
                 new Vector2(GetAimPoint(wpNumber).x, GetAimPoint(wpNumber).z), 
                 new Vector2(GetAimPoint(wayPoint.lastElement).x, GetAimPoint(wayPoint.lastElement).z));
-
             float currentDistance = Vector2.Distance(
                 new Vector2(aimPoint.x, aimPoint.z), 
-                new Vector2(transform.position.x, transform.position.z));
-
+                new Vector2(t.x, t.z));
             return (distanceBetweenLastAndNextAimPoint - currentDistance) / distanceBetweenLastAndNextAimPoint;
         }
         else
         {
+            var t = transform.position;
             float distanceBetweenLastAndNextAimPoint = Vector2.Distance(
                 new Vector2(GetAimPoint(wpNumber).x, GetAimPoint(wpNumber).z), 
                 new Vector2(GetAimPoint(wpNumber - 1).x, GetAimPoint(wpNumber - 1).z));
             float currentDistance = Vector2.Distance(
                 new Vector2(aimPoint.x, aimPoint.z), 
-                new Vector2(transform.position.x, transform.position.z));
+                new Vector2(t.x, t.z));
 
             return (distanceBetweenLastAndNextAimPoint - currentDistance) / distanceBetweenLastAndNextAimPoint;
         }
     }
-    bool NextWP() 
+    bool NextWp() 
     {
         return GetHorizontalDistanceToNextWayPoint(wayPointNumber) > .9f; 
     }
 
     void HandleNextTerrainTile() {
         Debug.Log("ChangeScene");
-        NextTile = Instantiate(terrainTiles, new Vector3(0, 0, terrain.terrainData.size.z), Quaternion.identity);
-        wayPoint = NextTile.GetComponentInChildren<WayPoint>();
+        nextTile = Instantiate(terrainTiles, new Vector3(0, 0, terrain.terrainData.size.z), Quaternion.identity);
+        wayPoint = nextTile.GetComponentInChildren<WayPoint>();
         wayPoint.Translate(tileNumber);
-        terrain = NextTile.GetComponentInChildren<Terrain>();
+        terrain = nextTile.GetComponentInChildren<Terrain>();
         tileNumber++;
         wayPointNumber = 0;
     }
