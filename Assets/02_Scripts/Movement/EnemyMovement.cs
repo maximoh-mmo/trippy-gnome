11cb907a9fa4cada11007c42f02f6680756b0926
max heinze <maxheinze@googlemail.com>
2024-01-14 16:50:12 +0100

    Dancer and Biter Movements

--- Assets/02_Scripts/Movement/EnemyMovement.cs
+++ Assets/02_Scripts/Movement/EnemyMovement.cs
@@ -6,8 +6,8 @@ public class EnemyMovement : MonoBehaviour
     public int movementPattern;
     public float moveToPlayerSpeed = 2;
     public float movementPatternSpeed = 1;
-    private Vector3 startPosition;
-    private float startTime;
+    private float startTime, circleSize = 0.03f;
+    private float circleGrowSpeed = 0.002f;
     private void Awake()
     {
         if (Random.Range(0, 2) > 0)
@@ -17,33 +17,37 @@ public class EnemyMovement : MonoBehaviour
         startTime = Time.time;
     }
 
-    public Vector3 ProcessMove(Vector3 currentPosition, Transform player)
+    public void ProcessMove(Transform player)
     {
-        if (movementPattern == 1) return Pattern1(currentPosition, player);
-        if (movementPattern == 2) return Pattern2(currentPosition, player);
-        return currentPosition;
+        if (movementPattern == 1) Pattern1(player);
+        if (movementPattern == 2) Pattern2(player);
+        startTime += 0.01f;
     }
 
-    //Pattern1 side to side movement based on sinecurve 
-    private Vector3 Pattern1(Vector3 currentPosition, Transform player)
+    //Pattern1 side to side movement based on sinecurve moving ever toward player
+    private void Pattern1(Transform player)
     {
-        var directionTowardPlayer = player.position - currentPosition;
+        var pPos = player.position;
+        var tPos = transform.position;
+        var directionTowardPlayer = pPos - tPos;
         var directionLeft = Vector3.Cross(directionTowardPlayer.normalized, Vector3.up).normalized;
         var leftMultipliedBySineCurve = directionLeft.normalized * (Mathf.Sin(startTime) * Time.deltaTime * movementPatternSpeed);
         var movementInPlayerDirection = directionTowardPlayer.normalized * (Time.deltaTime * moveToPlayerSpeed);
-        startTime += 0.01f;
-        return leftMultipliedBySineCurve + movementInPlayerDirection;
+        tPos += leftMultipliedBySineCurve + movementInPlayerDirection;
+        transform.position = tPos;
     }
     //pattern2 spiral
-    private Vector3 Pattern2(Vector3 currentPosition, Transform player)
+    private void Pattern2(Transform player)
     {
-        var directionTowardPlayer = player.position - currentPosition;
+        var pPos = player.position;
+        var tPos = transform.position;
+        var directionTowardPlayer = pPos - tPos;
         var directionLeft = Vector3.Cross(directionTowardPlayer.normalized, Vector3.up).normalized;
-        var rotationPoint = directionTowardPlayer;
-        // var direction = Quaternion.Euler(0, 0, 60) * direction;
-        // transform.Translate(direction.normalized * distanceThisFrame, Space.World);
-        // var distanceThisFrame = movementPatternSpeed * Time.deltaTime;
-        // var out = direction * distanceThisFrame;
-        return currentPosition;
+        var leftMultipliedBySineCurve = directionLeft.normalized * (Mathf.Sin(startTime) * circleSize);
+        var verticalMultipliedByCosineCurve = Vector3.up.normalized * (Mathf.Cos(startTime) * circleSize );
+        var movementInPlayerDirection = directionTowardPlayer.normalized * (Time.deltaTime * moveToPlayerSpeed);
+        circleSize -= circleGrowSpeed * Time.deltaTime;
+        tPos += (movementInPlayerDirection + leftMultipliedBySineCurve + verticalMultipliedByCosineCurve)* movementPatternSpeed;
+        transform.position = tPos;
     }
 }
