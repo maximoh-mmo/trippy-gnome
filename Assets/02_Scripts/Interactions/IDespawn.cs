9abadaee05f66e3c311c084f63f9ae8351b5d290
max heinze <maxheinze@googlemail.com>
2024-01-22 12:17:03 +0100

    update despawning method

--- Assets/02_Scripts/Interactions/IDespawn.cs
+++ Assets/02_Scripts/Interactions/IDespawn.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class IDespawn : MonoBehaviour
 {
+    private Camera mainCam;
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Dot(mainCam.transform.forward, transform.position - mainCam.transform.position) <= 0) Destroy(gameObject);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other != null && other.gameObject.name == "Sweeper") { Destroy(gameObject); } 
