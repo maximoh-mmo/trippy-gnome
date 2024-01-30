b7976b38ee7d782ca99ed1f7fef7d359d499476b
maximoh <58399612+maximoh-mmo@users.noreply.github.com>
2024-01-30 14:22:41 +0100

    audio ui added

--- Assets/02_Scripts/Interactions/SoundManager.cs
+++ Assets/02_Scripts/Interactions/SoundManager.cs
@@ -1,16 +1,46 @@
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour, IPlaySoundIfFreeSourceAvailable
 {
 
     AudioSource[] myQueue = new AudioSource[2];
+    [SerializeField] private AudioMixer masterMixer;
+    [SerializeField] Slider masterSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider SFXSlider;
     [SerializeField] AudioClip myClip;
     [SerializeField] float startingVolume = 1.5f;
     [SerializeField] float distortingSound = 2f;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        masterSlider.onValueChanged.AddListener(SetMainVolume);
+        SFXSlider.onValueChanged.AddListener(SetSFXVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        SetMusicVolume(1);
+        SetSFXVolume(1);
+        SetMusicVolume(1);    
+    }
+
     void Start()
     {
         myQueue = new AudioSource[2];
+    }
+
+    private void SetMainVolume(float value)
+    {
+        masterMixer.SetFloat("MasterVol", Mathf.Log10(value)*80);
+    }
+    private void SetMusicVolume(float value)
+    {
+        masterMixer.SetFloat("MusicVol", Mathf.Log10(value)*80);
+    }
+    private void SetSFXVolume(float value)
+    {
+        masterMixer.SetFloat("SFXVol", Mathf.Log10(value)*80);
     }
 
     // Update is called once per frame
@@ -27,16 +57,6 @@ public class SoundManager : MonoBehaviour, IPlaySoundIfFreeSourceAvailable
             temp.volume = startingVolume >= 1 ? startingVolume : distortingSound;
         }
     }
-
-    //public void PlayAudioOnFirstFreeAvailable(AudioClip myClip)
-    //{
-    //    if (!myQueue[0].isPlaying) { myQueue[0].clip = myClip; myQueue[0].Play(); }
-    //    else if (myQueue[0].isPlaying && !myQueue[1].isPlaying) { myQueue[1].clip = myClip; myQueue[1].Play(); }
-    //    else
-    //    {
-    //        Debug.Log(myClip.name + " could not been played on this Object: " + this.gameObject.name);
-    //    }
-    //}
     public void PlayAudioOnFirstFreeAvailable()
     {
         if (!myQueue[0].isPlaying)
